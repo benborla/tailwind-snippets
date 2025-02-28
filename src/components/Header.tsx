abb31fcbdade87ea1707bfb363677b27b28bc304
Ben Borla <benborla@icloud.com>
2025-02-28 16:08:25 +0800

    updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,8 +16,8 @@ export function Header() {
   const { data: session, status } = useSession();
 
   return (
-    <header className="sticky top-0 z-50 w-full max-w-6xl mx-auto border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container mx-auto flex h-14 items-center justify-center md:justify-end">
+    <header className="sticky top-0 z-50 w-full mx-auto border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="max-w-6xl container mx-auto flex h-14 items-center justify-center md:justify-end">
         <div className="flex items-center gap-4">
           <ModeToggle />
           {status === "authenticated" && session.user ? (
